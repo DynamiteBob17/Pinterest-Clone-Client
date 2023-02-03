8ca32740c80ea61cbf873db688f413d61eec237d
PickleEaterJim33 <jan.mlinaric@gmail.com>
2023-02-03 16:21:01 +0100

    feat: App.jsx done for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,113 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
+import PostPicForm from './components/PostPicForm';
+import PicGrid from './components/PicGrid';
+import { VIEW_ALL, VIEW_ME, VIEW_SPECIFIC, VIEW_POST } from './utils/constants';
 import './App.scss';
+const cookies = new Cookies();
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [user, setUser] = useState(null);
+  const [view, setView] = useState(VIEW_ALL);
+  const [userView, setUserView] = useState(-1);
+
+  const handleLogout = () => {
+    cookies.remove('gh_jwt');
+    window.location.reload();
+  };
+
+  const handleView = (view) => {
+    setView(view);
+  }
+
+  const handleUserView = (id) => {
+    setUserView(id);
+  }
+
+  useEffect(() => {
+    if (cookies.get('gh_jwt')) {
+      (async function () {
+        try {
+          const { data } =
+            await axios
+              .get(import.meta.env.VITE_SERVER_ORIGIN + '/auth/me', {
+                withCredentials: true,
+              });
+
+          setUser(data);
+          setUserView(data.id);
+        } catch (error) {
+          console.error(error);
+          cookies.remove('gh_jwt');
+        }
+      })();
+    }
+  }, []);
 
   return (
     <div className="App">
-      
+      <div className="top_bar">
+        <div className="top_left">
+          <button
+            onClick={() => handleView(VIEW_ALL)}
+            className={view === VIEW_ALL ? 'selected' : ''}
+          >
+            All
+          </button>
+          {
+            user &&
+            <>
+              <button
+                onClick={() => { handleView(VIEW_ME); handleUserView(user.id); }}
+                className={view === VIEW_ME ? 'selected' : ''}
+              >
+                My Pics
+              </button>
+              <button
+                onClick={() => handleView(VIEW_POST)}
+                className={view === VIEW_POST ? 'selected' : ''}
+              >
+                Post a Pic
+              </button>
+            </>
+          }
+        </div>
+        <div className="top_right">
+          {
+            user
+              ? <button
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
+              : <a
+                href={
+                  'https://github.com/login/oauth/authorize'
+                  + `?client_id=${import.meta.env.VITE_GH_CLIENT_ID}`
+                  + `&redirect_uri=${import.meta.env.VITE_SERVER_ORIGIN + '/auth/github'}`
+                  + '&path=/'
+                  + '&scope=profile'
+                }
+              >
+                <i className="fa-brands fa-github" /> Login
+              </a>
+          }
+        </div>
+      </div>
+      <div className="main">
+        {
+          view === VIEW_POST
+            ? <PostPicForm user={user} />
+            : <PicGrid
+              user={user}
+              view={view}
+              userView={userView}
+              handleView={handleView}
+              handleUserView={handleUserView}
+            />
+        }
+      </div>
     </div>
   );
 }
