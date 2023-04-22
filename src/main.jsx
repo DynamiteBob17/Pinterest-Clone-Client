29709cb1d6fa93709291df8703154299a4c4e3c5
Jan Mlinarić <jan.mlinaric@gmail.com>
2023-04-22 16:46:19 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import 'react-tooltip/dist/react-tooltip.css';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+//  <React.StrictMode>
+  <App />
+//  </React.StrictMode>,
 );
